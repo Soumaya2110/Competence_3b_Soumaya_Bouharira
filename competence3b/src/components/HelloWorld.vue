48b477cae8c9566736b14dfb779a9fa1e18c1a9d
Soumaya2110 <113605811+Soumaya2110@users.noreply.github.com>
2022-11-28 09:19:22 +0100

    1er commit

---
+++ competence3b/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <navbar></navbar>
+  </div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+
+
+
+export default {
+  name: 'HelloWorld',
+  components:{
+    'navbar': NavBar
+
+  }
+  
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
